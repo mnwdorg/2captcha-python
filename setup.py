ba6e5d883ad956f460d0e1d52564b2e125017751
mnwdbase <89862266+mnwdbase@users.noreply.github.com>
2021-12-19 14:10:44 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,13 +1,12 @@
 #!/usr/bin/env python3
 
 from setuptools import setup, find_packages
-from twocaptcha import __version__
 
 with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setup(name='2captcha-python',
-      version=__version__,
+      version='1.1.0',
       description='Python module for easy integration with 2Captcha API',
       long_description=long_description,
       long_description_content_type="text/markdown",
@@ -15,12 +14,11 @@ setup(name='2captcha-python',
       install_requires=['requests'],
       author='2Captcha',
       author_email='info@2captcha.com',
-      packages=find_packages(),
+      packages=find_packages(include=['twocaptcha']),
       include_package_data=True,
       classifiers=[
           "Programming Language :: Python :: 3",
           "License :: OSI Approved :: MIT License",
           "Operating System :: OS Independent",
       ],
-      python_requires='>=3.6',
-      test_suite='tests')
+      python_requires='>=3.6')
